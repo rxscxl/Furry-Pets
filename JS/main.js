a17113e1969169139f390a85c62742fe4747ea83
Daniel <gacenko.daniil@gmail.com>
2023-10-10 23:22:54 +0300

    header-fixed
    JS-header-fixed

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+/*----------------header-fixed----------------*/ 
+
+let header = document.querySelector('.js-header'),
+	headerH = document.querySelector('.js-header').clientHeight;
+
+document.onscroll = function(){
+	let scroll = window.scrollY;
+
+	if (scroll > headerH){
+		header.classList.add('fixed');
+		document.body.style.paddingTop = headerH + 'px'
+	}else {
+		header.classList.remove('fixed');
+		document.body.removeAttribute('style');
+	}
+};
